3c1073de27a12c9d1ef8e02b1a49871b4b604d83
rajatchaudhary-innoplexus <rajat.chaudhary@innoplexus.com>
2016-07-04 15:16:51 +0530

    Added the feature of downloading the converted excel file

--- routes/index.js
+++ routes/index.js
@@ -3,56 +3,59 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var mongoxlsx = require('mongo-xlsx');
 
-function get_data(db, callback){
-	var collection = db.collection('dummy');
-	collection.find().toArray(function(err, items){
+function get_data(db, collection, callback){
+	var coll = db.collection(collection);
+	coll.find().toArray(function(err, items){
 		// console.log(items[0]);
 		callback(items);
 		//convert here
 	});
 }
 
-var http = require('http');
-var fs = require('fs');
-
-var download_it = function(url, dest, cb) {
-	var file = fs.createWriteStream(dest);
-	var request = http.get(url, function(response) {
-		console.log("HTTP Request sent..");
-		response.pipe(file);
-		file.on('finish', function() {
-			file.close(cb);  // close() is async, call cb after close completes.
-		});
-	});
-}
-
-function convert_to_excel(items){
+function convert_to_excel(items, cb){
 	var model = mongoxlsx.buildDynamicModel(items);
 	// Generate Excel file
 	mongoxlsx.mongoData2Xlsx(items, model, function(err, data){
 		// console.log('File Saved at: ', data.fullPath);
 		console.log(data);
-		var url = "/home/rajat.choudhary/Documents/table_to_excel-pdf/table-to-excel-pdf/" + data.fileName;
-		download_it(url, data.fullPath, function(){
-			console.log("downloaded");
-		});
+		cb(data.fullPath);
 	})
 }
 
 /* GET home page. */
-router.get('/to_excel', function(req, res, next) {
-	MongoClient.connect("mongodb://localhost:27017/table_db", function(err, db){
-		if(err){ return console.dir(err);}
-		get_data(db, function(items){
-			// console.log(items[0]);
-			//convert here
-			convert_to_excel(items);
-		});
-		
+router.get('/', function(req, res, next){
+	res.render('index', {
+		title: 'Express',
+		isReady: false
 	});
-	res.render('index', { title: 'Express' });
+});
+router.post('/to_excel', function(req, res, next) {
+	console.log(req.body.db);
+	var url = "mongodb://localhost:27017/" + req.body.db;
+	MongoClient.connect(url, function(err, db){
+		if(err){ return console.dir(err);}
+		get_data(db, req.body.collection, function(items){
+			convert_to_excel(items, function(path){
+				console.log("Converted");
+				res.render('index', {
+					title: 'Express',
+					isReady: true,
+					path: path
+				});
+			});
+		});	
+	});
 });
 
+router.get('/download/:file(*)', function(req, res){
+	var path=require('path');
+	var file = req.params.file;
+	console.log(file);
+	var path = path.resolve(".")+'/'+file;
+	console.log(path);
+	console.log("Downloading Start");
+	res.download(path);
+});
 
 
 module.exports = router;
