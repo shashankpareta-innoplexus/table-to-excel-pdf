9084e86a1e77266f43b27ef08c1bde2c5255d90f
rajatchaudhary-innoplexus <rajat.chaudhary@innoplexus.com>
2016-07-04 16:20:49 +0530

    changed file name and remove unnecessary columns in excel file

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,30 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var mongoxlsx = require('mongo-xlsx');
 
-function get_data(db, collection, callback){
+function convert_data(db, collection, callback){
 	var coll = db.collection(collection);
 	coll.find().toArray(function(err, items){
-		// console.log(items[0]);
-		callback(items);
-		//convert here
+		for (var i = 0; i < items.length; i++) { 
+			if(items[i].hasOwnProperty("_id")){
+				delete items[i]['_id'];
+			}
+			if(items[i].hasOwnProperty("__v")){
+				delete items[i]['__v'];
+			}
+		}
+		var model = mongoxlsx.buildDynamicModel(items);
+		// Generate Excel file
+		mongoxlsx.mongoData2Xlsx(items, model, function(err, data){
+			console.log(data);
+			var fs = require('fs');
+			var new_name = collection+".xlsx";
+			console.log(new_name);
+			fs.rename(data.fileName, new_name, function(err) {
+				if ( err ) console.log('ERROR: ' + err);
+			});
+			callback(new_name);
+		});
 	});
-}
-
-function convert_to_excel(items, cb){
-	var model = mongoxlsx.buildDynamicModel(items);
-	// Generate Excel file
-	mongoxlsx.mongoData2Xlsx(items, model, function(err, data){
-		// console.log('File Saved at: ', data.fullPath);
-		console.log(data);
-		cb(data.fullPath);
-	})
 }
 
 /* GET home page. */
@@ -29,19 +36,19 @@ router.get('/', function(req, res, next){
 		isReady: false
 	});
 });
+
+
 router.post('/to_excel', function(req, res, next) {
 	console.log(req.body.db);
 	var url = "mongodb://localhost:27017/" + req.body.db;
 	MongoClient.connect(url, function(err, db){
 		if(err){ return console.dir(err);}
-		get_data(db, req.body.collection, function(items){
-			convert_to_excel(items, function(path){
-				console.log("Converted");
-				res.render('index', {
-					title: 'Express',
-					isReady: true,
-					path: path
-				});
+		convert_data(db, req.body.collection, function(path){
+			console.log("Converted");
+			res.render('index', {
+				title: 'Express',
+				isReady: true,
+				path: path,
 			});
 		});	
 	});
@@ -50,8 +57,8 @@ router.post('/to_excel', function(req, res, next) {
 router.get('/download/:file(*)', function(req, res){
 	var path=require('path');
 	var file = req.params.file;
-	console.log(file);
-	var path = path.resolve(".")+'/'+file;
+	console.log(file.slice(11));
+	var path = path.resolve(".")+'/'+ file;
 	console.log(path);
 	console.log("Downloading Start");
 	res.download(path);
